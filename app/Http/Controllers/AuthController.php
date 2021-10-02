ccb90454d13c9b982e6b4b503ba981508850a273
sonle <son.le@hdwebsoft.co>
2021-10-03 00:06:45 +0700

    add login logout

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+class AuthController extends Controller
+{
+    public function loginCreateToken (Request $request) {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $token = $user->createToken('fb-auto')->plainTextToken;
+        return ['token' => $token];
+    }
+
+    public function revokingTokens (Request $request) {
+        // Revoke all tokens...
+        return $request->user()->tokens()->delete();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,20 +14,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('revoking-tokens', 'AuthController@revokingTokens');
 
-Route::group(['prefix' => 'facebook'], function () {
-    Route::post('login', 'FacebookUsersController@login');
-    Route::get('get-content', 'FacebookUsersController@getPageContent');
-    Route::post('user-info', 'FacebookUsersController@getUserInfo');
-    Route::post('user-friends', 'FacebookUsersController@getUserFriends');
-    Route::get('users', 'FacebookUsersController@getListUsers');
-    Route::group(['prefix' => 'post'], function () {
-        Route::post('me', 'FacebookUsersController@post');
-        Route::post('get-comments', 'PostsController@getComments');
-        Route::post('get-all-comments', 'PostsController@getAllComments');
+    Route::group(['prefix' => 'facebook'], function () {
+        Route::post('login', 'FacebookUsersController@login');
+        Route::get('get-content', 'FacebookUsersController@getPageContent');
+        Route::post('user-info', 'FacebookUsersController@getUserInfo');
+        Route::post('user-friends', 'FacebookUsersController@getUserFriends');
+        Route::get('users', 'FacebookUsersController@getListUsers');
+        Route::group(['prefix' => 'post'], function () {
+            Route::post('me', 'FacebookUsersController@post');
+            Route::post('get-comments', 'PostsController@getComments');
+            Route::post('get-all-comments', 'PostsController@getAllComments');
+        });
     });
 });
+
+Route::post('/sanctum/token', 'AuthController@loginCreateToken');
+
+
 
