c5ebcc8961bd86f1a9dd73da57050bec686e596a
sonle <son.le@hdwebsoft.co>
2021-10-03 22:17:21 +0700

    get phone and email

--- app/Transformers/PostTransformer.php
+++ app/Transformers/PostTransformer.php
@@ -23,11 +23,16 @@ class PostTransformer extends TransformerAbstract
     {
         $result = array();
         foreach ($model as $comment) {
+            preg_match("/[(]*\d{3}[)]*\s*[.\-\s]*\d{3}[.\-\s]*\d{4}/", $comment['message'], $phone_matches);
+            preg_match("/[a-z0-9_\-\+\.]+@[a-z0-9\-]+\.([a-z]{2,4})(?:\.[a-z]{2})?/i", $comment['message'], $email_matches);
+
             $result[] = [
                 'fb_uid'         => $comment['from']['id'],
                 'name'         => $comment['from']['name'],
                 'comment_id'         => $comment['id'],
                 'message'         => $comment['message'],
+                'phone'         => !empty($phone_matches) ? $phone_matches[0] : null,
+                'email'         => !empty($email_matches) ? $email_matches[0] : null,
                 'created_time' => $comment['created_time'],
             ];
         }
