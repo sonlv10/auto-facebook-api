080ed8294706ec5cb496ce86b0a268a3fc71cc5d
sonle <son.le@hdwebsoft.co>
2021-10-03 08:50:25 +0700

    update sql

--- database/sql/v.1.0/dbpatch_0001.sql
+++ database/sql/v.1.0/dbpatch_0001.sql
@@ -3,15 +3,15 @@
 -- ----------------------------
 DROP TABLE IF EXISTS `facebook_users`;
 CREATE TABLE `facebook_users` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `fb_uid` varchar(255) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `avatar` varchar(512) DEFAULT NULL,
-  `cookies` json DEFAULT NULL,
-  `access_token` varchar(512) DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+`id` int unsigned NOT NULL AUTO_INCREMENT,
+`fb_uid` varchar(255) NOT NULL,
+`name` varchar(255) DEFAULT NULL,
+`avatar` varchar(512) DEFAULT NULL,
+`cookies` json DEFAULT NULL,
+`access_token` varchar(512) DEFAULT NULL,
+`created_at` timestamp NULL DEFAULT NULL,
+`updated_at` timestamp NULL DEFAULT NULL,
+PRIMARY KEY (`id`)
 );
 
 ALTER TABLE `auto_facebook_api`.`facebook_users`
@@ -22,9 +22,9 @@ ADD COLUMN `params` json NULL AFTER `cookies`;
 -- ----------------------------
 DROP TABLE IF EXISTS `facebook_posts`;
 CREATE TABLE `facebook_posts` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `post_id` int unsigned NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+`id` int unsigned NOT NULL AUTO_INCREMENT,
+`post_id` int unsigned NOT NULL,
+`created_at` timestamp NULL DEFAULT NULL,
+`updated_at` timestamp NULL DEFAULT NULL,
+PRIMARY KEY (`id`)
 );
